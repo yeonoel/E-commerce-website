ede381ea5e3933c67701f6204079e98129923448
= <=>
2023-05-05 22:43:28 +0000

    Creation of different folders, files, images, icons, colors of frontend and backend

---
+++ my_all_stars/src/datas/index.jsx
@@ -0,0 +1,93 @@
+import shoe1 from '../assets/1'
+import shoe2 from '../assets/2'
+import shoe3 from '../assets/3'
+import shoe4 from '../assets/4'
+import shoe5 from '../assets/5'
+import shoe6 from '../assets/6'
+import shoe7 from '../assets/7'
+import shoe8 from '../assets/7'
+
+
+
+export const shoes = [
+    {
+        id: '1ab',
+        cover: shoe1, 
+        status: true,
+        name:"All Star black white",
+        review: 4,
+        amount: 14,
+        description: "Inspired by the iconic looks of '90s girl bands, these platform Chucks are sure to turn heads. Canvas can be as refined or edgy as you'd like, while a double-stacked sole takes you higher."
+
+    },
+    {
+        id: '2ab',
+        cover: shoe2, 
+        status: true,
+        name:"All Star rose white",
+        review: 1,
+        amount: 40,
+        description: "Inspired by the iconic looks of '90s girl bands, these platform Chucks are sure to turn heads. Canvas can be as refined or edgy as you'd like, while a double-stacked sole takes you higher."
+
+    },
+    {
+        id: '3ab',
+        cover: shoe3, 
+        status: true,
+        name:"All Star red white",
+        review: 4,
+        amount: 90,
+        description: "Inspired by the iconic looks of '90s girl bands, these platform Chucks are sure to turn heads. Canvas can be as refined or edgy as you'd like, while a double-stacked sole takes you higher."
+
+    },
+    {
+        id: '4ab',
+        cover: shoe4, 
+        status: true,
+        name:"All Star black white",
+        review: 4,
+        amount: 27,
+        description: "Inspired by the iconic looks of '90s girl bands, these platform Chucks are sure to turn heads. Canvas can be as refined or edgy as you'd like, while a double-stacked sole takes you higher."
+
+    },
+    {
+        id: '5ab',
+        cover: shoe5, 
+        status: true,
+        name:"All Star rose white",
+        review: 1,
+        amount: 15,
+        description: "Inspired by the iconic looks of '90s girl bands, these platform Chucks are sure to turn heads. Canvas can be as refined or edgy as you'd like, while a double-stacked sole takes you higher."
+
+    },
+    {
+        id: '6ab',
+        cover: shoe6, 
+        status: true,
+        name:"All Star Black  Design",
+        review: 2,
+        amount: 19,
+        description: "Inspired by the iconic looks of '90s girl bands, these platform Chucks are sure to turn heads. Canvas can be as refined or edgy as you'd like, while a double-stacked sole takes you higher."
+
+    },
+    {
+        id: '7ab',
+        cover: shoe7,
+        status: true,
+        name:"All Star Black",
+        review: 2,
+        amount: 5,
+        description: "Inspired by the iconic looks of '90s girl bands, these platform Chucks are sure to turn heads. Canvas can be as refined or edgy as you'd like, while a double-stacked sole takes you higher."
+
+    },
+    {
+        id: '8ab',
+        cover: shoe8, 
+        status: true,
+        name:"All Star white try",
+        review: 1,
+        amount: 5,
+        description: "Inspired by the iconic looks of '90s girl bands, these platform Chucks are sure to turn heads. Canvas can be as refined or edgy as you'd like, while a double-stacked sole takes you higher."
+
+    }
+]
